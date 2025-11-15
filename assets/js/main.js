62a60f7120f333ebb95a7a8aeda37530cc1c02d9
Zenythic <gestionservicios@zenythic.com>
2025-11-15 13:33:49 -0500

    Update WhatsApp number in message handler

--- assets/js/main.js
+++ assets/js/main.js
@@ -516,7 +516,7 @@ class CasinoLanding {
     handleWhatsAppSubmit() {
         // Build WhatsApp message
         const message = 'Hola, quiero ser cajero, mi nombre es: '
-        const whatsappNumber = '+5493517512096';
+        const whatsappNumber = '+5493517512136';
         const whatsappUrl = `https://wa.me/${whatsappNumber.replace(/[^0-9]/g, '')}?text=${message}`;
         
         // Open WhatsApp
